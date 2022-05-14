a9baebb27ffe9a6fd6e04e59c2e0721b0772c727
Bllose <574931094@qq.com>
2022-05-14 16:30:37 +0800

    验证二叉搜索树算法

---
+++ src/main/java/bllose/binaryTree/BinaryTrees.java
@@ -0,0 +1,44 @@
+package bllose.binaryTree;
+
+public class BinaryTrees{
+
+    public static class TreeNode{
+        Integer val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(Integer val){this.val = val;}
+        TreeNode(Integer val, TreeNode left, TreeNode right){
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public boolean isValidBST(TreeNode root){
+        if(null == root) return true;
+
+        if(root.left != null){
+            if(!valid(root.left, null, root.val)) return false;
+        } 
+        if(root.right != null){
+            if(!valid(root.right, root.val, null)) return false;
+        }
+
+        return true;
+    }
+
+    private boolean valid(TreeNode root, Integer min, Integer max){
+        if(null == root) return true;
+
+        if(max != null && root.val >= max) return false;
+        if(min != null && root.val <= min) return false;
+
+        if(root.left != null){
+            if(!valid(root.left, min, root.val)) return false;
+        }
+        if(root.right != null){
+            if(!valid(root.right, root.val, max)) return false;
+        }
+        return true;
+    }
+}
