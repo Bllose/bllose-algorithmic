54ea77f86cdb1e80ceb2bab6537eabeeece725e3
Bllose Chen <574931094@qq.com>
2022-05-18 17:56:49 +0800

    Update Calculations.java

--- src/main/java/bllose/mathematical/Calculations.java
+++ src/main/java/bllose/mathematical/Calculations.java
@@ -4,6 +4,9 @@ import java.lang.Math;
 
 public class Calculations {
 
+    /**
+     * 将十进制小数转化为其他进制，保留十位小数
+     */
     public String decimalToOther(Double input, int radix){
         StringBuffer sb = new StringBuffer();
         sb.append("0.");
