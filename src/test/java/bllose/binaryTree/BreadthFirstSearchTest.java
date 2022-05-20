6fbb7a000ebf97749ddd7bd2622247870991fe21
Bllose Chen <574931094@qq.com>
2022-05-20 11:33:15 +0800

    Update BreadthFirstSearchTest.java

--- src/test/java/bllose/binaryTree/BreadthFirstSearchTest.java
+++ src/test/java/bllose/binaryTree/BreadthFirstSearchTest.java
@@ -20,4 +20,18 @@ public class BreadthFirstSearchTest {
         Assert.assertEquals("[5,6,4,null,null,1,8]",
                 JSONObject.toJSONString(TreeHelper.covertNode2Array(root)));
     }
+    
+    @Test
+    public void covertTest1(){
+        TreeNode root = TreeHelper.establishBinaryTree(new Integer[]{1,null,2,null, null, null, 3, null,null,null,null,null,null,null,4});
+        Assert.assertEquals("[1,null,2,null,null,null,3,null,null,null,null,null,null,null,4]",
+                JSONObject.toJSONString(TreeHelper.covertNode2Array(root)));
+    }
+
+    @Test
+    public void covertTest2(){
+        TreeNode root = TreeHelper.establishBinaryTree(new Integer[]{1,2,null,3,null,null,null,4});
+        Assert.assertEquals("[1,2,null,3,null,null,null,4]",
+                JSONObject.toJSONString(TreeHelper.covertNode2Array(root)));
+    }
 }
