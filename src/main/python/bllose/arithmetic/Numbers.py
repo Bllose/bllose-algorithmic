ff7e8c0b9f0978175fe7b21252eb8f8995af45cd
Bllose Chen <574931094@qq.com>
2022-06-15 20:26:07 +0800

    Update Numbers.py

--- src/main/python/bllose/arithmetic/Numbers.py
+++ src/main/python/bllose/arithmetic/Numbers.py
@@ -1,32 +1,65 @@
+package bllose.leetcode;
 
-def solution(target:str):
-    left = target[0:1]
-    right = target[1:2]
+public class DiffBST {
 
-    target = target[2:]
-    while len(target) > 0:
-        sum = int(left) + int(right)
-        sum = str(sum)
-        if target.startswith(sum):
-            left = right
-            right = sum
-            target = target[len(sum):]
-        else:
-            break
+    /**
+     * 		执行耗时:0 ms,击败了100.00% 的Java用户
+     * 		内存消耗:38 MB,击败了75.93% 的Java用户
+     * @param n
+     * @return
+     */
+    public static int numTrees(int n){
+        int[] G = new int[n+1];
+        G[0] = 1;
+        G[1] = 1;
 
-    if len(target) > 0:
-        print('False')
-    else:
-        print('True')
+        for(int i = 2; i <= n; ++i){
+            for(int j = 1; j <= i; ++j){
+                if(i-j != j-1){
+                    G[i] += G[j - 1] * G[i - j];
+                }else{
+                    G[i] *= 2;
+                    G[i] += G[j - 1] * G[i - j];
+                    break;
+                }
+                if(i- 2*j + 1 == 1) {
+                    G[i] *= 2;
+                    break;
+                }
+            }
+        }
+        return G[n];
+    }
 
-'''
-除了第一位数和第二位数以外，
-每一位数字都是前两位数字的和。
+    /**
+     * 			执行耗时:0 ms,击败了100.00% 的Java用户
+     * 			内存消耗:38.4 MB,击败了12.99% 的Java用户
+     * 			
+     * @param n
+     * @return
+     */
+    public static int numTreesNormal(int n){
+        int[] G = new int[n+1];
+        G[0] = 1;
+        G[1] = 1;
 
-现在判断输入的一串数字是否符合如上规则，符合则True; 否则 False
-'''
-if __name__ == '__main__':
-    solution('123')
-    solution('1236')
-    solution('1910')
-    solution('19101929')
+        for(int i = 2; i <= n; ++i){
+            for(int j = 1; j <= i; ++j){
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+
+    public static void main(String[] args) {
+        System.out.println("2 = " + DiffBST.numTrees(2));
+        System.out.println("5 = " + DiffBST.numTrees(3));
+        System.out.println("14 = " + DiffBST.numTrees(4));
+        System.out.println("42 = " + DiffBST.numTrees(5));
+
+        System.out.println("2 = " + DiffBST.numTreesNormal(2));
+        System.out.println("5 = " + DiffBST.numTreesNormal(3));
+        System.out.println("14 = " + DiffBST.numTreesNormal(4));
+        System.out.println("42 = " + DiffBST.numTreesNormal(5));
+    }
+}
